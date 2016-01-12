5c4fd47daaecb76f1fcd44f69f19dfe9380565da
Hackbright-student <info@hackbrightacademy.com>
2016-01-11 17:27:23 -0800

    using try/except to allow user to quit without error

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -3,7 +3,7 @@ filename = sys.argv[1]
 
 
 def alpha_sort_dictionary(filename):
-    """Given a file of restaurant data, prints an alpha-sorted list of restaurant name and rating. """
+    """Given a file of restaurant data, prints an alpha-sorted list of restaurant name and rating."""
 
     restaurant_data = open(filename)
     ratings = {}
@@ -26,8 +26,9 @@ def alpha_sort_dictionary(filename):
         print item + " is rated at " + ratings[item] + "."
 
 def sort_user_reported_rating(filename):
-    """"""
-    user_name = raw_input("Hello. What's your name?>> ")
+    """Given a file of restaurant data, asks user for restaurant name and rating and updates data."""
+    
+    raw_input("Hello. What's your name?>> ")
 
     restaurant_data = open(filename)
     ratings = {}
@@ -39,10 +40,24 @@ def sort_user_reported_rating(filename):
     
     restaurant_data.close()
 
-    random_restaurant = ratings.popitem()
-    print random_restaurant[0] + " is rated at " + random_restaurant[1] + "."
-    # user_rating = raw_input("")
+    while True: 
+            random_restaurant = ratings.popitem()
+            print random_restaurant[0] + " is rated at " + random_restaurant[1] + "."
+            try:
+                user_rating = int(raw_input("How would you rate {}? ".format(random_restaurant[0])))
+            except ValueError:
+                # if user_rating == 'q':
+                ratings[random_restaurant[0]] = random_restaurant[1]
+                break
+            ratings[random_restaurant[0]] = user_rating
+
+    keys = sorted(ratings)
+
+    #iterate through to print restaurants & ratings alphabetically
+    for item in keys:
+        # print item + " is rated at " + ratings[item] + "."
+        print"{} is rated at {}.".format(item, ratings[item])
 
 
-alpha_sort_dictionary(filename)
+# alpha_sort_dictionary(filename)
 sort_user_reported_rating(filename)
